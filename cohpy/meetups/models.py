6359fc54fdc0febedf6c78212fa1b03dfa29458a
janmilosh <janmilosh@gmail.com>
2015-07-11 14:06:21 -0400

    Adds to the meetups model

--- cohpy/meetups/models.py
+++ cohpy/meetups/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.utils.safestring import mark_safe
 
-# Create your models here.
+
+class Meetup(models.Model):
+    title = models.CharField()
+    description = models.TextField(blank=True)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=200)    
+    talks = models.ManyToManyField(Talk)
+    
+    def safe_description(self):
+        '''http://stackoverflow.com/questions/2080559/disable-html-escaping-in-djangos-textfield''' 
+        return mark_safe(self.description)    
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('date',)
+
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=64)
+    date_added = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class Talk(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    meetup = models.ForeignKey(Meetup)
+    speakers = models.ManyToManyField(Speaker)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
