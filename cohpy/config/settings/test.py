8fada52262fc8f0782217d00a3e110d0553d053f
janmilosh <janmilosh@gmail.com>
2015-07-16 22:29:42 -0400

    Sets up environment variables correctly - finally

--- cohpy/config/settings/test.py
+++ cohpy/config/settings/test.py
@@ -1,2 +1,15 @@
-from .dev import *
+import os
 
+from .base import *
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cohpy',
+    }
+}
