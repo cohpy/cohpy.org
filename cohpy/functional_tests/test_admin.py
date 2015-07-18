c192523a7dfe881a01edc8a69a9a797a2fbfef24
janmilosh <janmilosh@gmail.com>
2015-07-18 17:43:45 -0400

    Attempted setup for phantomjs tests on CI

--- cohpy/functional_tests/test_admin.py
+++ cohpy/functional_tests/test_admin.py
@@ -5,15 +5,15 @@ from selenium import webdriver
 # For reference: https://pytest.org/latest/fixture.html#fixture
 # http://www.pydanny.com/pytest-no-boilerplate-testing-2.html
 @pytest.fixture # setup and teardown
-def ff_browser(request):
-    browser = webdriver.Firefox()
-    browser.get('http://localhost:8000/admin')
+def browser(request):
+    driver = webdriver.PhantomJS()
+    driver.get('http://localhost:8000/admin')
     
     @request.addfinalizer
     def teardown():
-        browser.close()
+        driver.close()
     
-    return browser
+    return driver
 
-def test_navigates_to_admin_login(ff_browser):
-    assert 'Log in | Django site admin' in ff_browser.title
+def test_navigates_to_admin_login(browser):
+    assert 'Log in | Django site admin' in browser.title
