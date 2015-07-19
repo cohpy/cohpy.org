cd7060d7925364b74d6f5d5572c1d55ef55ebdb5
janmilosh <janmilosh@gmail.com>
2015-07-19 19:39:19 -0400

    Adds passing test for home_page view

--- cohpy/meetups/test_views.py
+++ cohpy/meetups/test_views.py
@@ -1,6 +1,5 @@
 from django.core.urlresolvers import resolve
-
-import pytest
+from django.http import HttpRequest
 
 from meetups.views import home_page
 
@@ -8,3 +7,10 @@ from meetups.views import home_page
 def test_root_url_resolves_to_home_page():
     found = resolve('/')
     assert found.func == home_page
+
+def test_home_page_returns_correct_html():
+    request = HttpRequest()
+    response = home_page(request)
+    assert response.content.startswith(b'<html>')
+    assert b'<title>COhPy | Home</title>' in response.content
+    assert response.content.endswith(b'</html>')
