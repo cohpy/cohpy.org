47fce59031bec4f0511abe03f26b5107d530db8e
janmilosh <janmilosh@gmail.com>
2015-08-15 12:43:21 -0400

    Improves resources tests

--- cohpy/python_resources/test_models.py
+++ cohpy/python_resources/test_models.py
@@ -6,11 +6,12 @@ from python_resources.pytest_fixtures import *
 
 
 @pytest.mark.django_db
-def test_category_creation(category):
-    assert isinstance(category, Category)
-    assert category.__str__() ==  'Tutorial'
+def test_category_creation(category1):
+    assert isinstance(category1, Category)
+    assert category1.__str__() ==  'Tutorial'
 
 @pytest.mark.django_db
-def test_python_resource_creation(python_resource):
-    assert isinstance(python_resource, PythonResource)
-    assert python_resource.__str__() ==  'Learn Python the Hard Way'
+def test_python_resource_creation(python_resource1):
+    assert isinstance(python_resource1, PythonResource)
+    assert python_resource1.__str__() ==  'Learn Python the Hard Way'
+    assert python_resource1.comma_separated_categories() == 'Book, Tutorial, Video'
